6beb84f890198090acd62817621e652a94bd29ce
Daniel Ghirasim <daniel.ghirasim@evozon.com>
2022-10-27 12:34:44 +0300

    init commit

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict'
+
+// // Variable is not declared only a string is assigned to it
+// pateu = 'bucegi'
+
+// // Reserved Names
+// let debugger;
+// let double;
+// let public;
+// let char;
+
+// // Will throw error
+// function foo(a, a) {
+//     return a + b;
+// }
+
+
+// Getters are not allowed to be changed
+//  let obj = {get x() {return 0} };
+ 
+//  // Will throw an error
+//  obj.x = 3.14;    
